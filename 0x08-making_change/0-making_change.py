105a2ff23e39ba2048bf1edf22f0c16d342ddf77
emmanuelist <emmanuel.paul75@yahoo.com>
2024-07-24 18:26:37 +0100

    feat(algorithms): Implement dynamic programming solution for Coin Change problem

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
 """
-Main file for testing
 """
 
 
-def makeChange(coins, amount):
+def makeChange(coins, total):
     """
-    How many of this type of coin can I get with my money? Okay,
-        I'll take that many. Now, how much money do I have left?
-        And how many coins do I have in my pocket?
+    This function calculates the minimum number of coins required to make a
+    given total amount using a given set of coin denominations.
+    It uses dynamic programming to efficiently solve this problem.
+
+    Parameters:
+    coins (list of int): A list of coin denominations.
+    Each element in the list represents a coin denomination.
+    total (int): The total amount to be made using the given
+    coin denominations.
+
+    Returns:
+    int: The minimum number of coins required to make the total amount.
+    If it is not possible to make the total amount using the given coin
+    denominations, it returns -1.
     """
-    if amount < 1:
+    if total <= 0:
         return 0
-    coins.sort(reverse=True)
-    count = 0
+    dp = [0] + [float("inf")] * (total)
     for coin in coins:
-        if amount == 0:
-            break
-        num = amount // coin
-        amount -= num * coin
-        count += num
-    return count if amount == 0 else -1
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[-1] if dp[-1] != float("inf") else -1
