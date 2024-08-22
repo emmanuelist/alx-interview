14b349c41c0140a0f6737b90456f71c9613d41cd
emmanuelist <emmanuel.paul75@yahoo.com>
2024-08-22 12:42:15 +0100

    feat: Implement UTF-8 validation function
    - Added the `validUTF8` function in the `utf8validator` module to validate a sequence of bytes as a valid UTF-8 encoding.
    - The function processes a list of integers representing bytes and checks whether the sequence adheres to UTF-8 encoding rules.
    - It supports multi-byte characters and ensures that continuation bytes are correctly prefixed with '10'.
    - Returns `True` for valid UTF-8 sequences and `False` for invalid sequences.
    - Includes examples in the docstring to demonstrate usage.
    
    This implementation improves data validation by ensuring only correctly encoded UTF-8 sequences are accepted.

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+"""
+utf8validator module
+
+This module provides a function to validate a sequence of bytes as a valid
+UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """
+    Validate a sequence of bytes as a valid UTF-8 encoding.
+
+    Args:
+        data (list[int]): A list of integers representing bytes in the range
+        0-255.
+
+    Returns:
+        bool: True if the input data is a valid UTF-8 encoding, False
+        otherwise.
+
+    Example:
+        >>> validUTF8([197, 130, 1])  # valid UTF-8 encoding for "Á"
+        True
+        >>> validUTF8([250, 130, 1])  # invalid UTF-8 encoding
+        False
+    """
+    num_bytes = 0
+
+    for num in data:
+        bin_rep = format(num, '#010b')[-8:]
+
+        if num_bytes == 0:
+            if bin_rep[0] == '0':
+                continue
+            elif bin_rep[:3] == '110':
+                num_bytes = 1
+            elif bin_rep[:4] == '1110':
+                num_bytes = 2
+            elif bin_rep[:5] == '11110':
+                num_bytes = 3
+                return False
+        else:
+            if bin_rep[:2] != '10':
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
